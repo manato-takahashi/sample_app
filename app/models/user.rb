a766c6128ef1401939e83444ea2fbfb028200283
Manato Takahashi <154682191+manato-takahashi@users.noreply.github.com>
2024-03-14 08:22:58 +0000

    add: ユーザー名とメールアドレスに対するバリデーション 演習まで完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  # 上記は validates(:name, presence: true) と書いても同じ
-  validates :email, presence: true
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  # 上記は validates(:name, presence: true ...) と書いても同じ
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,12 +11,48 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "name should be present" do
-    @user.name = "   "
+    @user.name = "a" * 51
     assert_not @user.valid?
   end
 
   test "email should be present" do
-    @user.email = "   "
+    @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+  # 有効なメールアドレスへバリデーションが機能するかのテスト
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp 
+    alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid? "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  # 無効なメールアドレスへバリデーションが機能するかのテスト
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com 
+    foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  # 重複するメールアドレス拒否のテスト
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  # メールアドレスの小文字化に対するテスト
+  test "email addresses should be saved as lowercase" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+  
 end
