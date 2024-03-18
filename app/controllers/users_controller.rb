347e5c208acbcb666d1663d29c01f04d460dc42c
Manato Takahashi <154682191+manato-takahashi@users.noreply.github.com>
2024-03-18 04:41:53 +0000

    add: paramsの中にadmin属性が含まれていることを確認する演習まで完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params) # 実装は終わっていないことに注意！
+    if @user.save
+      # 保存の成功をここで扱う
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
