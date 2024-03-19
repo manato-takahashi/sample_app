25956faf481cd12685a47b8934fd91818cafb356
Manato Takahashi <154682191+manato-takahashi@users.noreply.github.com>
2024-03-19 14:36:07 +0000

    add: Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.alert-danger'
   end
+
+  # 有効なユーザーを登録しようとした際に、登録されてUser.countに登録前と後で1だけ差分があることを
+  # 確認するテスト
+  test 'valid signup information' do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Example User',
+                                         email: 'user@example.com',
+                                         password: 'password',
+                                         password_confirmation: 'password' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
