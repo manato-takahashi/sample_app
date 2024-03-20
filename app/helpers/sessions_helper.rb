1480ceaf128d8bc01c80081516b49cef8d41e3c9
Manato Takahashi <154682191+manato-takahashi@users.noreply.github.com>
2024-03-20 07:28:13 +0000

    add: ログイン中のユーザーのレイアウトのリンクを変更する

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,4 +10,9 @@ module SessionsHelper
       @current_user ||= User.find_by(id: session[:user_id]) # ||= は += などと同じ意味
     end
   end
+
+  # ユーザーがログインしていればtrue, そうでないならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
