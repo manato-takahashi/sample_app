2e4202e8ca1ed3fe1a638e12daa705815ec6b65a
Manato Takahashi <154682191+manato-takahashi@users.noreply.github.com>
2024-03-20 02:40:00 +0000

    add: sessions_controllerを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  root "static_pages#home"
-  get "/help",     to: "static_pages#help"
-  get "/about",    to: "static_pages#about"
-  get "/contact",  to: "static_pages#contact"
-  get "/signup",   to: "users#new"
+  root 'static_pages#home'
+  get '/help',     to: 'static_pages#help'
+  get '/about',    to: 'static_pages#about'
+  get '/contact',  to: 'static_pages#contact'
+  get '/signup',   to: 'users#new'
+  get '/login',    to: 'sessions#new'
+  get '/login',    to: 'sessions#create'
+  get '/logout',   to: 'sessions#destroy'
   resources :users
 end
