863129e0de8392f9fda5c0a4c14f3119012fe957
Manato Takahashi <154682191+manato-takahashi@users.noreply.github.com>
2024-03-23 07:27:51 +0000

    add: rememberメソッドをUserモデルに追加し、その後の演習まで完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token # 仮想の属性を作成
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   # 上記は validates(:name, presence: true ...) と書いても同じ
@@ -9,10 +10,23 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 8 }
 
-  # 渡された文字列のハッシュ値を返す
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+    # 渡された文字列のハッシュ値を返す
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # ランダムなトークンを返す
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    # 永続化セッションのためにユーザーをデータベースに記憶する
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
   end
 end
